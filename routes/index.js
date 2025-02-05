331e69db564889dedda3a208df6cd169303f9d29
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2025-02-05 14:46:25 +0500

    upadted index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const tesseract = require("node-tesseract-ocr");
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = express.Router();
+
+// Set Tesseract path early in the code
+process.env.TESSERACT_PATH = "D:\\Software\\tesseract\\tesseract.exe";
+console.log("Tesseract Path:", process.env.TESSERACT_PATH); // To verify if the path is set correctly
+
+// Tesseract configuration
+const config = {
+  lang: "eng",
+  oem: 1,
+  psm: 3
+};
+
+
+// Route to fetch and convert the image
+router.post("/", async (req, res) => {
+  const imageUrl = `${req.body.imageUrl}`;
+
+  console.log("Using Static Image Path:", imageUrl);
+
+  try {
+    const text = await tesseract.recognize(imageUrl, config);
+    console.log("OCR Result:", text);
+    res.send(text);
+  } catch (error) {
+    console.error("OCR Error:", error.message);
+    res.status(500).send("Error processing the image.");
+  }
 });
+
+
+// tesseract.recognize("D:\\Learning\\Web Development\\MileStone Project\\imageExtractor-backend\\public\\images\\englishpronouns.png", config)
+//   .then(text => {
+//     console.log("OCR Result:", text);
+//   })
+//   .catch(error => {
+//     console.error("OCR Error:", error.message);
+//   });
 
 module.exports = router;
